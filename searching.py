0830d85787f4c377f2d12283aab9739230eb0da0
Tymur Talalaievskyi <timmthaler7@gmail.com>
2026-04-16 15:34:15 +0200

    Načtení dat ze souboru

--- searching.py
+++ searching.py
@@ -1,31 +1,23 @@
 from pathlib import Path
 import json
 
-
 def read_data(file_name, field):
-    """
-    Reads a JSON file and returns data for a given field.
-
-    Args:
-        file_name (str): Name of the JSON file.
-        field (str): Key to retrieve from the JSON data.
-            Must be one of: 'unordered_numbers', 'ordered_numbers' or 'dna_sequence'.
-
-    Returns:
-        list | str | None:
-            - list: If data retrieved by the selected field contains numeric data.
-            - str: If field is 'dna_sequence'.
-            - None: If the field is not supported.
-    """
-    # get current working directory path
     cwd_path = Path.cwd()
-    
     file_path = cwd_path / file_name
 
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    allowed_fields = ["unordered_numbers", "ordered_numbers", "dna_sequence"]
+
+    if field not in allowed_fields:
+        return None
+
+    return data.get(field)
 
 def main():
-    pass
-
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 if __name__ == "__main__":
     main()
